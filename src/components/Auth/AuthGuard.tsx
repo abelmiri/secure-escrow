564e7717bfad093472fab770d3ce5283766706c1
Abel <miri1888@gmail.com>
2026-06-01 17:41:07 +0330

    dev mode changes

--- src/components/Auth/AuthGuard.tsx
+++ src/components/Auth/AuthGuard.tsx
@@ -10,6 +10,12 @@ interface AuthGuardProps {
 
 export default function AuthGuard({ children }: AuthGuardProps) {
   const { isLoggedIn, authState } = useUser()
+  const isDev = process.env.NODE_ENV === "development"
+
+  // If in development mode, bypass the safeguard
+  if (isDev) {
+    return <>{children}</>
+  }
 
   // Show nothing while checking authentication
   if (authState?.isLoading) {
