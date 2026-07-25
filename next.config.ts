9f091513b3faee52a7c313ba23b60975dab6f963
Abel <miri1888@gmail.com>
2026-07-25 23:53:00 +0330

    env values added to next

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  env: {
+    PHASE: process.env.PHASE,
+    DEV_USER: process.env.DEV_USER,
+  },
 }
 
 export default nextConfig
